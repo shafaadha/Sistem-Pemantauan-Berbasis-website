c7c3daa0264c3d875d2ad3e1415bc1df9a349402
shafaadha <adhashafa@gmail.com>
2024-05-31 00:58:10 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -21,14 +21,3 @@ use App\Http\Controllers\API\ValidationController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/valid-In', [AccessController::class, 'processIn']);
-
-Route::post('/valid-Out', [AccessController::class, 'processOut']);
-
-Route::get('/slot', [SlotController::class, 'slotCount']);
-
-Route::post('/cekqr_in', [ValidationController::class, 'scanIn']);
-Route::post('/cekqr_out', [ValidationController::class, 'scanOut']);
-Route::post('/plat_in', [ValidationController::class, 'platIn']);
-Route::post('/plat_out', [ValidationController::class, 'platOut']);
-
